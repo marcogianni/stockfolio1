1fa84bad393ca56d42528ae74905a7c851ad53c0
Marco Gianni <gian93ma50@gmail.com>
2023-09-14 09:05:55 +0200

    cc

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,21 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
 
-import type { NextRequest } from "next/server";
-import type { Database } from "@/lib/database.types";
+import type { NextRequest } from 'next/server'
+import type { Database } from '@/lib/database.types'
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient<Database>({ req, res });
-  await supabase.auth.getSession();
-  return res;
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient<Database>({ req, res })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  // if user is signed in and the current path is / redirect the user to /dashboard
+  if (user && req.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', req.url))
+  }
+
+  return res
 }
